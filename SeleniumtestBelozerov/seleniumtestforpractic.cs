3b009428d1c6f500b9eb61ebd74df5217a7b6b58
BelozerovAndrey <elusivepusher@gmail.com>
2024-04-15 19:25:05 +0500

    убрали ожидание

--- SeleniumtestBelozerov/seleniumtestforpractic.cs
+++ SeleniumtestBelozerov/seleniumtestforpractic.cs
@@ -6,35 +6,44 @@ namespace SeleniumtestBelozerov;
 
 public class seleniumtestforpractic
 {
-    [Test]
-    public void authorization()
-    {
-       // 231456
+        public ChromeDriver
+
+        [Test]
+        public void authorization()
+        {
+                // 231456
 
 // зайти в хром  (с помощью вебдрайвера)
-        var driver = new ChromeDriver();
+                var driver = new ChromeDriver();
 
 // перейти по урлу https://staff-testing.testkontur.ru
-driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
-Thread.Sleep(millisecondsTimeout:5000);
+                driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
+                Thread.Sleep(millisecondsTimeout: 5000);
 
 // ввести логин и пароль
-        var login = driver.FindElement(By.Id("Username"));
-        login.SendKeys(text:"a.belozerov@skbkontur.ru");
-        var password = driver.FindElement(By.Name("Password"));
-        password.SendKeys("xPtqNJbcomETL7sSDnaCRMKr@"); 
-        Thread.Sleep(millisecondsTimeout:5000);  
+                var login = driver.FindElement(By.Id("Username"));
+                login.SendKeys(text: "a.belozerov@skbkontur.ru");
+                var password = driver.FindElement(By.Name("Password"));
+                password.SendKeys("xPtqNJbcomETL7sSDnaCRMKr@");
+                Thread.Sleep(millisecondsTimeout: 5000);
 // нажать на кнопку "войти"
-        var Enter = driver.FindElement(By.Name("button")); 
-        Enter.Click();
-        Thread.Sleep(millisecondsTimeout:5000);  
+                var Enter = driver.FindElement(By.Name("button"));
+                Enter.Click();
+
 
 // 7
 
 // проверяем что мы находимся на нужной странице 
-            var currentURL = driver.Url;
-Assert.That( currentURL == "https://staff-testing.testkontur.ru/news2");
+                var currentURL = driver.Url;
+                Assert.That(currentURL == "https://staff-testing.testkontur.ru/news2");
 // закрываем браузер и убиваем процесс драйвера
-driver.Quit();
-    }
+
+                [TearDown]
+                public void TearDown()
+                {
+                        driver.Quit();
+
+                }
+
+        }
 }
