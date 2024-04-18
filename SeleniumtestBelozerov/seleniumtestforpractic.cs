c502a640631fb7448b324e1fe07210a03b2ba1b6
BelozerovAndrey <elusivepusher@gmail.com>
2024-04-19 02:01:36 +0500

    Делаю первый  тест, недоделал.

--- SeleniumtestBelozerov/seleniumtestforpractic.cs
+++ SeleniumtestBelozerov/seleniumtestforpractic.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Runtime.InteropServices;
+using FluentAssertions;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -11,57 +12,46 @@ public class SeleniumTestsForPractic
 
     public ChromeDriver driver;
 
-    [Test]
+    [Test] //проверяем авторизацию
     public void Authorization()
+    {
+        Autorization();
+        var news = driver.FindElement(By.CssSelector("[data-tid='Title']")); 
+        var currentUrl = driver.Url;
+        currentUrl.Should().Be("https://staff-testing.testkontur.ru/news");
+    }
+
+    [Test]
+    public void CommunityСreation() //создание сообщества
+    {
+        //Авторизация
+        Autorization();
+        var communities = driver.FindElement(By.CssSelector("[data-tid='Community']"));
+        communities.Click();
+        Thread.Sleep(3000);
+        var Creation  = driver.FindElement(By.XPath("/html/body/div/section/section[2]/section/div[2]/span/button"));
+        Creation.Click();
+        Thread.Sleep(3000);
+        var NameCommunites  = driver.FindElement(By.XPath("/html/body/div[2]/div/div[2]/div/div/div/div/div[3]/div[2]/div/span/div/div[2]/div[1]/span/label/div/div/textarea"));
+        NameCommunites.SendKeys("Тестирую создание сообщества");
+        Thread.Sleep(3000);
+    }
+
+    public void Autorization()
     {
         var options = new ChromeOptions();
         options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
-
-// - зайти в хром ( с помощью вебдрайвера)
         driver = new ChromeDriver(options);
-        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); // неявное ожидание
-       // var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2)); явное ожидание и скок тип ожидает, каждые 2 секунды например.
-
-// - перейти по урлу https://staff-testing.testkontur.ru
-
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); 
         driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
-
-
-
-// - ввести логин и пароль
         var login = driver.FindElement(By.Id("Username"));
         login.SendKeys("a.belozerov@skbkontur.ru");
-
         var password = driver.FindElement(By.Name("Password"));
         password.SendKeys("xPtqNJbcomETL7sSDnaCRMKr@");
-
-// - нажать на кнопку "войти"
         var enter = driver.FindElement(By.Name("button"));
         enter.Click();
-        var news = driver.FindElement(By.CssSelector("[data-tid='Title']"));
-        //wait.Until(ExpectedConditions.UrlContains("https://staff-testing.testkontur.ru/news")); // я ожидаю, что после клика у меня урл(UrlContains) будет содержать новость, работает только с var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));  
-
-// ожидание
-//Thread.Sleep(3000); //плохой вариант
-
-
-// var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3)); - явное ожидание
-// wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("Username")));
-
-// - проверяем что мы находимся на нужной странице
-        var currentUrl = driver.Url;
-        currentUrl.Should().Be("https://staff-testing.testkontur.ru/news");
-
-
-                //Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news","current url = " + currentUrl + " а должен быть https://staff-testing.testkontur.ru/news");
-
-//currentUrl.Should().Be("https://staff-testing.testkontur.ru/news");
-
-// закрываем браузер и убиваем процесс драйвера
-
-    }
-
-    [TearDown]
+        }
+[TearDown]
     public void TearDown()
     {
         driver.Quit();
