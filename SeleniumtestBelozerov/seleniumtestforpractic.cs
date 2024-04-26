6eb313ea0f72c41a1cd12710545ec8903088ff5a
BelozerovAndrey <elusivepusher@gmail.com>
2024-04-27 00:20:20 +0500

    1. Добавлен Сетап
    2. Работа с драйвером вынесена в сетап
    3. Thread.Sleep убраны
    4. Нечеловекочитаемые "Ypravleniesoobshestvom" убраны
    5. Сложные сценарии заменены на более простые

--- SeleniumtestBelozerov/seleniumtestforpractic.cs
+++ SeleniumtestBelozerov/seleniumtestforpractic.cs
@@ -12,10 +12,20 @@ public class SeleniumTestsForPractic
 
     public ChromeDriver driver;
 
+    [SetUp] // крутая штукенция которая активируется в каждый запущенный тест.
+    public void Setup()
+    { 
+        var options = new ChromeOptions();
+        options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+        driver = new ChromeDriver(options);
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(6);
+        Autorization();
+        
+    }
     [Test] //проверяем авторизацию
     public void Authorization()
     {
-        Autorization();
+        
         var news = driver.FindElement(By.CssSelector("[data-tid='Title']")); 
         var currentUrl = driver.Url;
         currentUrl.Should().Be("https://staff-testing.testkontur.ru/news");
@@ -24,122 +34,91 @@ public class SeleniumTestsForPractic
     [Test] // Проверяем создание сообщества
     public void CommunityСreation() 
     {
-        //Авторизация
-        Autorization();
-        //создание сообщества (вынес код, т.к. еще в удалении будут те же строки)
-        HostCommunity();
+        
+        CreationCommunity(); //создание сообщества 
         
         
-        var Ypravleniesoobshestvom = driver.FindElement(By.CssSelector("[data-tid='Title']"));
-        Ypravleniesoobshestvom.Should().NotBeNull(); // проверяем, что сообщество создалось и мы попали в упарвление сообществом.
+        var CommunityManagement = driver.FindElement(By.CssSelector("[data-tid='Title']"));
+        CommunityManagement.Should().NotBeNull(); // проверяем, что сообщество создалось и мы попали в упарвление сообществом.
 
     }
     [Test] //Проверяем удаление сообщества
     public void CommunityDelete() 
+     {
+        
+        CreationCommunity(); //создание сообщества
+        var DeleteButton1 = driver.FindElement(By.CssSelector("[data-tid='DeleteButton']"));
+        DeleteButton1.Click();
+        var DeleteButtonexpectation  = driver.FindElement(By.CssSelector(".react-ui-button-caption"));
+        var DeleteButton2  = driver.FindElement(By.CssSelector(".react-ui-button-caption"));
+        DeleteButton2.Click();
+        var newspage = driver.FindElement(By.CssSelector("[data-tid='PageHeader']")); //проверяем, что удаление произошло и нас переместило на старницу новостей
+        
+        
+        
+     }
+
+    [Test] //проверяем, что работает переход в настройки
+    public void Setting() 
     {
-        //Авторизация
-        Autorization();
-        //создание сообщества
-        HostCommunity();
-        var DeleteButton = driver.FindElement(By.CssSelector("[data-tid='DeleteButton']"));
-        DeleteButton.Click();
-        Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
-        var KnopkaYdalit  = driver.FindElement(By.CssSelector(".react-ui-button-caption"));
-        KnopkaYdalit.Click();
-        Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
-        //тут должно быть что-то проверяющее удаление сообщества? Пока не придумал, буду думать :с
+        var Avatar = driver.FindElement(By.CssSelector("[data-tid='Avatar']"));
+        Avatar.Click();
+        var Settingsexpectation = driver.FindElement(By.CssSelector("[data-tid='Settings']"));
+        var Settings = driver.FindElement(By.CssSelector("[data-tid='Settings']"));
+        Settings.Click();
+        var SettingsPage = driver.FindElement(By.CssSelector("[data-tid='ModalPageBody']"));//проверяем, что мы в нужном месте.
+     
+        
+        
     }
 
-    [Test] // Проверяем создание обсуждения
-    public void CreateDiscussion() 
+    [Test] // Проверяем срабатывание кнопки "закрыть" в настройках сообщества.
+    public void CloseButtonсClick() 
     
     {
-//Авторизация
-        Autorization();
-//создание сообщества
-        HostCommunity();
-        //создание обсуждения
-        CreateaDiscussion();
+        CreationCommunity(); //создание сообщества
         
-        var ProverkaSozdaniyaSoobshestva   = driver.FindElement(By.CssSelector("[data-tid='Link']"));
-        ProverkaSozdaniyaSoobshestva.Should().NotBeNull(); //проверяем, что обсуждение создалось
+        
+    
+          var closedbuttonexpectation = driver.FindElement(By.CssSelector("[fill-rule='evenodd']"));  
+            var closedbutton = driver.FindElement(By.XPath("//*[@id=\"root\"]/section/section[2]/section/div/button"));
+            closedbutton.Click();
+            var CommunityViewPage = driver.FindElement(By.CssSelector("[clip-rule='evenodd']"));
+            CommunityViewPage.Should().NotBeNull();// проверяем, что закрылось и мы в нужном месте.
+            
+       
     }
+   
 
-    [Test] //Проверяем удаление обсуждения
-    public void DeleteDiscussion() 
-    {
-        //Авторизация
-        Autorization();
-//создание сообщества
-        HostCommunity();
-        //создание обсуждения
-        CreateaDiscussion();
-        var Discussion   = driver.FindElement(By.CssSelector("[data-tid='Link']"));
-        Discussion.Click();
-        Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
-        var Menu   = driver.FindElement(By.XPath("//*[@id=\"root\"]/section/section[2]/div/div/div/section/div[2]/div[2]/div/div/div/span/button"));
-        Menu.Click();
-        Thread.Sleep(5000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания 
-        var DeleteRecord = driver.FindElement(By.CssSelector("[data-tid='DeleteRecord']"));
-        DeleteRecord.Click();
-        Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
-        var DeleteButton = driver.FindElement(By.CssSelector("[data-tid='DeleteButton']"));
-        DeleteButton.Click();
-        Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
-        var NoDiscussion = driver.FindElement(By.XPath("//*[@id=\"root\"]/section/section[2]/div/div/div/section/div/div/div"));
-        NoDiscussion.Should().NotBeNull(); //проверяем, что дискуссий нет
-    }
-
-    public void CreateaDiscussion()
-    {
-        var closed = driver.FindElement(By.XPath("//*[@id=\"root\"]/section/section[2]/section/div/button"));
-        closed.Click();
-        Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
-        var Discussions =
-            driver.FindElement(By.XPath("//*[@id=\"root\"]/section/section[2]/div/div/section/div[2]/div[1]/a[2]"));
-        Discussions.Click();
-        Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
-        var HostDiscussion =
-            driver.FindElement(
-                By.XPath("//*[@id=\"root\"]/section/section[2]/div/div/div/section/span/button/div[2]/span[2]"));
-        HostDiscussion.Click();
-        Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
-        var NameDiscussion = driver.FindElement(By.CssSelector("[data-tid='Name']"));
-        NameDiscussion.SendKeys("Название обсуждения");
-        Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
-        var MesageDiscussion = driver.FindElement(By.CssSelector(".public-DraftEditor-content"));
-        MesageDiscussion.SendKeys("Сообщение обсуждения");
-        Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
-        var CreateButton = driver.FindElement(By.CssSelector("[data-tid='CreateButton']"));
-        CreateButton.Click();
-        Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
-    }
-
-    public void HostCommunity()
+  
+    public void CreationCommunity() //метод создание сообщества
 {
     var communities = driver.FindElement(By.CssSelector("[data-tid='Community']"));
     communities.Click();
-    Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
+    var ButtonCreation = driver.FindElement(By.CssSelector("[fill-rule='evenodd']"));
+    
     var Creation  = driver.FindElement(By.XPath("/html/body/div/section/section[2]/section/div[2]/span/button"));
     Creation.Click();
-    Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
+    var fieldNameCommunites = driver.FindElement(By.CssSelector("[placeholder='Название сообщества']"));
     var NameCommunites  = driver.FindElement(By.XPath("/html/body/div[2]/div/div[2]/div/div/div/div/div[3]/div[2]/div/span/div/div[2]/div[1]/span/label/div/div/textarea"));
     NameCommunites.SendKeys("Название сообщества");
-    Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
+    var fieldcommunitydescription  = driver.FindElement(By.CssSelector("[placeholder='Описание сообщества']"));
+    fieldcommunitydescription.Click();
+    fieldcommunitydescription.SendKeys(text: "описание сообщества");
     var CreationButter  = driver.FindElement(By.CssSelector(".react-ui-m0adju"));
     CreationButter.Click();
-    Thread.Sleep(3000); //понимаю,что сильные и независимые пограмисты их не используют, буду менять на другие ожидания
+    
+    
+
     
 }
 
 
 
-    public void Autorization()
+    public void Autorization() //метод авторизаци
     {
-        var options = new ChromeOptions();
-        options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
-        driver = new ChromeDriver(options);
-        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); 
+        
+         
         driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
         var login = driver.FindElement(By.Id("Username"));
         login.SendKeys("a.belozerov@skbkontur.ru");
